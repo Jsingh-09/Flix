b57d4399c3ec40fa13a211043752cae893a80faf
Jashandeep Singh <jashanbhinder2@gmail.com>
2020-10-02 19:21:50 -0700

    update week 2

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -70,14 +70,32 @@ override func viewDidLoad() {
         return cell
     }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        print("Loading up the details screen")
+        
+        // Find the selected movie
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        
+        let movie = movies[indexPath.row]
+        
+        
+        
+        
+        
+        // Pass the selected movie to the details view controller
+        
+        let detialsViewController = segue.destination as! MovieDetailsViewController
+        detialsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
 
 }
